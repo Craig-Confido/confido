bc548497ee7b70b25257e5411143d01df2c08232
Karl <karl@kejk.tech>
2020-09-03 00:34:43 +0100

    fix: carousel?

--- src/views/About.vue
+++ src/views/About.vue
@@ -47,8 +47,8 @@
       <h3>Success rate</h3>
     </v-col>
   </v-row>
-  <v-row>
-    <v-carousel cycle class="mx-0" height="400" hide-delimiter-background light :show-arrows="false" no-gutters>
+  <v-row class="mx-0white--text" no-gutters">
+    <v-carousel cycle class="mx-0" height="400" hide-delimiter-background light :show-arrows="false">
       <v-carousel-item v-for="quote in quotes" :key="quote">
         <v-sheet color="#F1F1FF" height="100%">
           <v-row class="fill-height px-16 mx-0 mx-md-16" align="center" justify="center">
