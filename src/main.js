b44c4ee116eee1feada5326b2c050af661b0c892
Karl Koch <hi@kejk.co>
2020-11-17 19:55:25 +0000

    fix: added additional vue-analytics to ensure it works optimally across Vue views

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
-import './registerServiceWorker'
-import 'vue-material-design-icons/styles.css'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import router from './router';
+import './registerServiceWorker';
+import 'vue-material-design-icons/styles.css';
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: 'UA-117426148-1'
+});
 
 new Vue({
   vuetify,
