87195621d83faa43a76a45fb7b712c8724565b0e
Grant Simmonds <grant.simmonds@gmail.com>
2020-11-23 14:51:51 +0000

    Dev - GTM implementation  (#1)
    * add git ignore so git is setup correctly && remove vue-ga as not being used && does not work && install vuew tag manager implementattion
    
    * remove OLD google analytics setup and implement GTM setup
    
    Co-authored-by: Grant Simmonds <grant@trueup.com>

--- src/main.js
+++ src/main.js
@@ -1,23 +1,32 @@
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import VueAnalytics from 'vue-ua';
+// import VueAnalytics from 'vue-ua';
+import VueTagManager from "vue-tag-manager"
+
+
 import router from './router';
 import './registerServiceWorker';
 import 'vue-material-design-icons/styles.css';
 
+
 Vue.config.productionTip = false;
 
-Vue.use(VueAnalytics, {
-  // [Required] The name of your app as specified in Google Analytics.
-  appName: 'Confido (new)',
-  // [Required] The version of your app.
-  appVersion: '1.0',
-  // [Required] Your Google Analytics tracking ID.
-  trackingId: 'G-PH3Y6TK0PQ',
-  // If you're using vue-router, pass the router instance here.
-  vueRouter: router,
-});
+// Vue.use(VueAnalytics, {
+//   // [Required] The name of your app as specified in Google Analytics.
+//   appName: 'Confido (new)',
+//   // [Required] The version of your app.
+//   appVersion: '1.0',
+//   // [Required] Your Google Analytics tracking ID.
+//   trackingId: 'GTM-M5TFCP2',
+//   // If you're using vue-router, pass the router instance here.
+//   vueRouter: router,
+// });
+
+// Install VUE GTM -> this will handle all analytics and marketing tags
+Vue.use(VueTagManager, {
+  gtmId: 'GTM-M5TFCP2'
+})
 
 new Vue({
   vuetify,
