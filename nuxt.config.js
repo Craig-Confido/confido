e3e8ac29c1ced204c206c6b55da975602daebe20
Karl Koch <karl@kejk.tech>
2021-11-21 14:34:23 +0000

    fix: error

--- nuxt.config.js
+++ nuxt.config.js
@@ -112,7 +112,7 @@ export default {
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [{ src: "~/plugins/hotjar.js", mode: "client" }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
